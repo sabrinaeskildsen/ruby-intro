3a2375e0da4beb5bc48fc82296ceca4e25194a66
sabrinaeskildsen <sabrina.eskildsen@kellogg.northwestern.edu>
2023-01-24 20:49:04 +0000

    class exercise

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -1,15 +1,83 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 2-conditionals.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 2-conditionals.rb
 
-# Booleans
+# # Booleans
+# is_true = true
+# is_false = false
+# puts is_true
+# puts is_false
 
-# Boolean Expressions
+# # Boolean Expressions
+# puts 3 > 2
+# puts 3 < 2
+# puts 3 == 2
+# puts 3 != 2
 
-# If Conditional Logic
+# # If Conditional Logic
+# if true
+#     puts "awesome"
+# end
 
-# If/Else Conditional Logic
+# if 3 > 2
+#     puts "awesome"
+#     puts "math works!"
+# end
 
-# Elsif Conditional Logic
+# if 3 < 2
+#     puts "what?!?!?!"
+# end
+
+# # If/Else Conditional Logic
+# if 3 > 2
+#     puts "awesome"
+#     puts "math works!"
+# else
+#     puts "what????!!!!"
+# end
+
+# password = "Tacos"
+# user_entered_password = "password1"
+
+# if password == user_entered_password
+#     puts "you're in!"
+# else
+#     puts "go away!"
+# end
+
+# # Elsif Conditional Logic
+
+# home_team_score = 101
+# away_team_score = 101
+
+# if home_team_score > away_team_score
+#     puts "winner!"
+# elsif home_team_score < away_team_score
+#     puts "loser :("
+# else
+#     puts "tied meh..."
+# end
 
 # Combining Expressions
+
+temp = 68
+
+if temp < 80
+    if temp > 60
+        puts "it's beautiful"
+    end
+else
+    puts "brrr"
+end
+
+if temp < 80 && temp > 60
+    puts "it's beautiful"
+else
+    puts "brrr"
+end
+
+if temp < 80 || temp > 60
+    puts "it's beautiful"
+else
+    puts "brrr"
+end
